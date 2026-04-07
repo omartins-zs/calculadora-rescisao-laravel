90471a7eecd72bf722f5b14b5515021609b9d656
omartins-zs <gabrielmatheus209@gmail.com>
2026-04-07 01:23:25 -0300

    :wrench: chore: definindo rotas publicas e web scraping

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RescisaoController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/calculadora-rescisao-trabalhista');
 });
+
+Route::get('/calculadora-rescisao-trabalhista', [RescisaoController::class, 'index']);
+Route::redirect('/calculadora-rescisao-clt', '/calculadora-rescisao-trabalhista', 301);
+
+Route::get('/como-calcular-rescisao-trabalhista', [RescisaoController::class, 'comoCalcular']);
+Route::get('/multa-fgts-40-por-cento', [RescisaoController::class, 'multaFgts']);
+Route::get('/multa-artigo-477-clt', [RescisaoController::class, 'multaArtigo477']);
+
+Route::post('/api/calculo/rescisao', [RescisaoController::class, 'calculate']);
