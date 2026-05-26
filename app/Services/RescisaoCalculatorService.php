071679389ec99b335349ba5de08dc2865419deb4
omartins-zs <gabrielmatheus209@gmail.com>
2026-05-26 18:45:06 -0300

    :bug: fix: tratando campo fgts vazio como nulo

--- app/Services/RescisaoCalculatorService.php
+++ app/Services/RescisaoCalculatorService.php
@@ -3,14 +3,17 @@
 namespace App\Services;
 
 use DateTime;
-use Exception;
 
 class RescisaoCalculatorService
 {
     protected AvisoPrevioService $avisoService;
+
     protected FeriasService $feriasService;
+
     protected DecimoTerceiroService $decimoTerceiroService;
+
     protected FgtsService $fgtsService;
+
     protected DescontosService $descontosService;
 
     public function __construct(
@@ -32,15 +35,15 @@ class RescisaoCalculatorService
         $salarioBase = floatval($input['salario_base_mensal']);
         $variavelMedia = floatval($input['salario_variavel_media_12m'] ?? 0);
         $adicionais = floatval($input['adicionais_mensais'] ?? 0);
-        
+
         $baseRemuneracao = $salarioBase + $variavelMedia + $adicionais;
-        
+
         $dataAdmissao = $input['data_admissao'];
         $dataDesligamentoOriginal = $input['data_desligamento'];
         $motivo = $input['motivo_rescisao'];
         $tipoAviso = $input['tipo_aviso_previo'] ?? 'nao_sei';
         $diasFaltas = intval($input['faltas_injustificadas_no_mes'] ?? 0);
-        
+
         $dependentes = intval($input['dependentes_ir'] ?? 0);
 
         // Prepara objeto de dados de saída
@@ -55,23 +58,23 @@ class RescisaoCalculatorService
 
         if ($tipoAviso === 'indenizado') {
             if ($motivo === 'dispensa_sem_justa_causa' || in_array($motivo, ['rescisao_antecipada_experiencia_empregador'])) {
-                $valorAviso = ($baseRemuneracao / 30) * $diasAviso;
+                $valorAviso = round(($baseRemuneracao / 30) * $diasAviso, 2);
                 $items[] = ['categoria' => 'Indenizações', 'nome' => 'Aviso Prévio Indenizado', 'referencia' => "$diasAviso dias", 'valor' => $valorAviso, 'isento' => true];
                 $totalBruto += $valorAviso;
-                
+
                 // Projeta tempo de aviso para data final
                 $d = new DateTime($dataDesligamentoProjetada);
                 $d->modify("+$diasAviso days");
                 $dataDesligamentoProjetada = $d->format('Y-m-d');
             } elseif ($motivo === 'acordo_entre_as_partes') {
-                $valorAviso = (($baseRemuneracao / 30) * $diasAviso) / 2; // Metade do aviso
+                $valorAviso = round((($baseRemuneracao / 30) * $diasAviso) / 2, 2);
                 $items[] = ['categoria' => 'Indenizações', 'nome' => 'Aviso Prévio Indenizado (Metade)', 'referencia' => "$diasAviso dias", 'valor' => $valorAviso, 'isento' => true];
                 $totalBruto += $valorAviso;
                 $d = new DateTime($dataDesligamentoProjetada);
                 $d->modify("+$diasAviso days");
                 $dataDesligamentoProjetada = $d->format('Y-m-d');
             } elseif ($motivo === 'pedido_de_demissao') {
-                $avisoDescontoEmpregado = ($baseRemuneracao / 30) * 30; // Desconta 30 dias se não cumpriu
+                $avisoDescontoEmpregado = round(($baseRemuneracao / 30) * 30, 2); // Desconta 30 dias se não cumpriu
                 $items[] = ['categoria' => 'Descontos', 'nome' => 'Desconto Aviso Prévio não cumprido', 'referencia' => '30 dias', 'valor' => $avisoDescontoEmpregado, 'isento' => false];
                 $totalDescontos += $avisoDescontoEmpregado;
             }
@@ -87,7 +90,7 @@ class RescisaoCalculatorService
         }
 
         if ($diasTrabalhados > 0) {
-            $valorSaldoSalario = ($baseRemuneracao / 30) * $diasTrabalhados;
+            $valorSaldoSalario = round(($baseRemuneracao / 30) * $diasTrabalhados, 2);
             $items[] = ['categoria' => 'Salários', 'nome' => 'Saldo de Salário', 'referencia' => "$diasTrabalhados/30 dias", 'valor' => $valorSaldoSalario, 'isento' => false];
             $totalBruto += $valorSaldoSalario;
         } else {
@@ -98,7 +101,7 @@ class RescisaoCalculatorService
         // Vencidas
         if (($input['possui_ferias_vencidas'] ?? 'nao') === 'sim') {
             $qtd = intval($input['qtd_periodos_ferias_vencidas'] ?? 1);
-            $valorFeriasVencidas = ($baseRemuneracao * $qtd) + (($baseRemuneracao * $qtd) / 3);
+            $valorFeriasVencidas = round(($baseRemuneracao * $qtd) + (($baseRemuneracao * $qtd) / 3), 2);
             $items[] = ['categoria' => 'Férias', 'nome' => 'Férias Vencidas + 1/3', 'referencia' => "$qtd período(s)", 'valor' => $valorFeriasVencidas, 'isento' => true];
             $totalBruto += $valorFeriasVencidas;
         }
@@ -109,7 +112,7 @@ class RescisaoCalculatorService
             // Para simplificar, num cenário real com base apenas na data de admissão e demissão:
             $adm = new DateTime($dataAdmissao);
             $desl = new DateTime($dataDesligamentoProjetada);
-            
+
             // Acha o último "aniversário" de contratação
             $ultimoAniverFerias = clone $adm;
             while ($ultimoAniverFerias < clone $desl->modify('-1 year')) {
@@ -120,10 +123,10 @@ class RescisaoCalculatorService
             }
 
             $avosFerias = $this->feriasService->calcularAvosProporcionais($ultimoAniverFerias->format('Y-m-d'), $dataDesligamentoProjetada);
-            
+
             if ($avosFerias > 0) {
-                $valorFeriasProp = ($baseRemuneracao / 12) * $avosFerias;
-                $terco = $valorFeriasProp / 3;
+                $valorFeriasProp = round(($baseRemuneracao / 12) * $avosFerias, 2);
+                $terco = round($valorFeriasProp / 3, 2);
                 $totalProp = $valorFeriasProp + $terco;
                 $items[] = ['categoria' => 'Férias', 'nome' => 'Férias Proporcionais + 1/3', 'referencia' => "$avosFerias/12 avos", 'valor' => $totalProp, 'isento' => true];
                 $totalBruto += $totalProp;
@@ -135,7 +138,7 @@ class RescisaoCalculatorService
         if ($motivo !== 'dispensa_com_justa_causa') {
             $avos13 = $this->decimoTerceiroService->calcularAvosDecimoTerceiro($dataDesligamentoProjetada);
             if ($avos13 > 0) {
-                $valor13 = ($baseRemuneracao / 12) * $avos13;
+                $valor13 = round(($baseRemuneracao / 12) * $avos13, 2);
                 $items[] = ['categoria' => '13º', 'nome' => '13º Salário Proporcional', 'referencia' => "$avos13/12 avos", 'valor' => $valor13, 'isento' => false];
                 $totalBruto += $valor13;
             }
@@ -144,13 +147,13 @@ class RescisaoCalculatorService
         // 5. EXTRAS / ADICIONAIS / OUTROS
         $outrosProventos = floatval($input['outros_proventos'] ?? 0);
         if ($outrosProventos > 0) {
-            $items[] = ['categoria' => 'Indenizações', 'nome' => 'Outros Proventos', 'referencia' => "-", 'valor' => $outrosProventos, 'isento' => false];
+            $items[] = ['categoria' => 'Indenizações', 'nome' => 'Outros Proventos', 'referencia' => '-', 'valor' => $outrosProventos, 'isento' => false];
             $totalBruto += $outrosProventos;
         }
-        
+
         $adiantamentoSalarial = floatval($input['adiantamento_salarial'] ?? 0);
         if ($adiantamentoSalarial > 0) {
-            $items[] = ['categoria' => 'Descontos', 'nome' => 'Adiantamento Salarial', 'referencia' => "-", 'valor' => $adiantamentoSalarial, 'isento' => false];
+            $items[] = ['categoria' => 'Descontos', 'nome' => 'Adiantamento Salarial', 'referencia' => '-', 'valor' => $adiantamentoSalarial, 'isento' => false];
             $totalDescontos += $adiantamentoSalarial;
         }
 
@@ -161,34 +164,36 @@ class RescisaoCalculatorService
         // INSS e IRRF sobre Saldo Salario
         if ($valorSaldoSalario > 0) {
             $resInssSalario = $this->descontosService->calcularINSS($valorSaldoSalario);
-            $inssSalario = $resInssSalario['desconto'];
+            $inssSalario = round($resInssSalario['desconto'], 2);
             if ($inssSalario > 0) {
-                $items[] = ['categoria' => 'Descontos', 'nome' => 'INSS (sobre Saldo Salário)', 'referencia' => "Base R$ " . number_format($valorSaldoSalario, 2, ',', '.'), 'valor' => $inssSalario, 'isento' => false];
+                $items[] = ['categoria' => 'Descontos', 'nome' => 'INSS (sobre Saldo Salário)', 'referencia' => 'Base R$ '.number_format($valorSaldoSalario, 2, ',', '.'), 'valor' => $inssSalario, 'isento' => false];
                 $totalDescontos += $inssSalario;
             }
 
             $baseIrSalario = $valorSaldoSalario - $inssSalario;
             $resIrSalario = $this->descontosService->calcularIRRF($baseIrSalario, $dependentes, $aplicarSimplificado, $aplicarRedutor2026);
-            if ($resIrSalario['desconto'] > 0) {
-                $items[] = ['categoria' => 'Descontos', 'nome' => 'IRRF (sobre Saldo Salário)', 'referencia' => "-", 'valor' => $resIrSalario['desconto'], 'isento' => false];
-                $totalDescontos += $resIrSalario['desconto'];
+            $impostoSalario = round($resIrSalario['desconto'], 2);
+            if ($impostoSalario > 0) {
+                $items[] = ['categoria' => 'Descontos', 'nome' => 'IRRF (sobre Saldo Salário)', 'referencia' => '-', 'valor' => $impostoSalario, 'isento' => false];
+                $totalDescontos += $impostoSalario;
             }
         }
 
         // INSS e IRRF sobre 13o
         if ($valor13 > 0) {
             $resInss13 = $this->descontosService->calcularINSS($valor13);
-            $inss13 = $resInss13['desconto'];
+            $inss13 = round($resInss13['desconto'], 2);
             if ($inss13 > 0) {
-                $items[] = ['categoria' => 'Descontos', 'nome' => 'INSS (sobre 13º Salário)', 'referencia' => "Base R$ " . number_format($valor13, 2, ',', '.'), 'valor' => $inss13, 'isento' => false];
+                $items[] = ['categoria' => 'Descontos', 'nome' => 'INSS (sobre 13º Salário)', 'referencia' => 'Base R$ '.number_format($valor13, 2, ',', '.'), 'valor' => $inss13, 'isento' => false];
                 $totalDescontos += $inss13;
             }
 
             $baseIr13 = $valor13 - $inss13;
             $resIr13 = $this->descontosService->calcularIRRF($baseIr13, $dependentes, $aplicarSimplificado, $aplicarRedutor2026);
-            if ($resIr13['desconto'] > 0) {
-                $items[] = ['categoria' => 'Descontos', 'nome' => 'IRRF (sobre 13º Salário)', 'referencia' => "-", 'valor' => $resIr13['desconto'], 'isento' => false];
-                $totalDescontos += $resIr13['desconto'];
+            $imposto13 = round($resIr13['desconto'], 2);
+            if ($imposto13 > 0) {
+                $items[] = ['categoria' => 'Descontos', 'nome' => 'IRRF (sobre 13º Salário)', 'referencia' => '-', 'valor' => $imposto13, 'isento' => false];
+                $totalDescontos += $imposto13;
             }
         }
 
@@ -198,16 +203,16 @@ class RescisaoCalculatorService
         $mesesTrabalhados = max(1, ($strAdmissao->diff($strDeslig)->y * 12) + $strAdmissao->diff($strDeslig)->m);
         // Regra burra de estimativa global para quem nao preencheu (salarioBase * mesesTrabalhados)
         $estimativaHistorica = $baseRemuneracao * $mesesTrabalhados;
-        
+
         $baseFgtsMes = $valorSaldoSalario; // Simplificado. Aviso indenizado nao entra FGTS aqui
-        $saldoInformado = ($input['saldo_fgts_para_fins_rescisorios'] ?? null) !== null && $input['saldo_fgts_para_fins_rescisorios'] !== '' 
-                          ? floatval($input['saldo_fgts_para_fins_rescisorios']) 
+        $saldoInformado = ($input['saldo_fgts_para_fins_rescisorios'] ?? null) !== null && $input['saldo_fgts_para_fins_rescisorios'] !== ''
+                          ? floatval($input['saldo_fgts_para_fins_rescisorios'])
                           : null;
 
         $fgtsParams = $this->fgtsService->calcular(
-            $baseFgtsMes, 
-            $motivo, 
-            $saldoInformado, 
+            $baseFgtsMes,
+            $motivo,
+            $saldoInformado,
             $estimativaHistorica
         );
 
@@ -227,9 +232,9 @@ class RescisaoCalculatorService
                 'premissas_ativas' => [
                     'Aviso prévio indenizado isento de IR/INSS (Padrão)',
                     'Férias indenizadas/proporcionais isentas de IR/INSS (Padrão)',
-                    'Simplificação IR e Redutor 2026 habilitados'
-                ]
-            ]
+                    'Simplificação IR e Redutor 2026 habilitados',
+                ],
+            ],
         ];
     }
 }
